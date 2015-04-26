1f99d228757652b220f07ffd99db319379cd5c16
Elena Chernousova <wertic@gmail.com>
2015-04-26 09:55:49 -0700

    R programming - second assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions.
 
-## Write a short comment describing this function
+##First one is implementation a matrix 
+##which allows to saved inverse matrix additionally with otriginal.       
 
+##Second illustrates using a cashed value of inverse matrix. 
+##The first call of "solve" takes the same amount of time like a regular function.
+##However, second call for the funtion "solve"
+##would be faster than first due using a chashed value.e
+
+## Creating matrix for inversion
 makeCacheMatrix <- function(x = matrix()) {
-
+    im <- NULL
+    set <- function(y) {
+        x <<- y
+        im <<- NULL
+    }
+    get <- function() x
+    setInverseMatrix <- function(inverseMatrix) im <<- inverseMatrix
+    getInverseMatrix <- function() im
+    list(set = set, get = get,
+         setInverseMatrix = setInverseMatrix,
+         getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
-
+## Invoke a function to get an inverse matrix.   
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    im <- x$getInverseMatrix()
+    if(!is.null(im)) {
+        message("getting cached data")
+        return(im)
+    }
+    data <- x$get()
+    im <- solve(data, ...)
+    x$setInverseMatrix(im)
+    im
 }
+
